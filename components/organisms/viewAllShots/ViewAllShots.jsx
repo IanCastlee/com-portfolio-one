cd7a58c8bf2b4de710305df24ddd10eae505c836
IanCastlee <castillo321ian@gmail.com>
2025-08-26 21:42:26 +0800

    changs

--- components/organisms/viewAllShots/ViewAllShots.jsx
+++ components/organisms/viewAllShots/ViewAllShots.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function ViewAllShots() {
-  return <div>ViewAllShots</div>;
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita maiores
+      libero possimus modi autem minus! Aut, cumque eligendi alias sint porro
+      saepe dolorum sed molestias. Illum, labore quis? Reprehenderit maiores
+      sapiente distinctio, a aspernatur illum numquam laborum at quisquam
+      consectetur magnam vero magni deleniti cumque earum nihil natus itaque
+      iusto?
+    </div>
+  );
 }
 
 export default ViewAllShots;
